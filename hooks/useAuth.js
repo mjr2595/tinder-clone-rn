757aece0a9b374d50a12c2722c2da89b9b109713
mjr2595 <michaeljohnraymond@gmail.com>
2023-03-25 00:05:58 -0500

    primero

---
+++ hooks/useAuth.js
@@ -0,0 +1,16 @@
+import { View, Text } from "react-native";
+import React, { createContext, useContext } from "react";
+import * as Google from "expo-google-app-auth";
+
+const AuthContext = createContext({});
+
+export const AuthProvider = ({ children }) => {
+  const signInWithGoogle = async () => {
+    await Google.logInAsync();
+  };
+  return <AuthContext.Provider value={{ user: null }}>{children}</AuthContext.Provider>;
+};
+
+export default function useAuth() {
+  return useContext(AuthContext);
+}
